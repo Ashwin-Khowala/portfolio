0a6e2a1d0e2350f6a24942e28406c3de723e201f
Ashwin-Khowala <ashwinkhowala1@gmail.com>
2025-01-29 20:43:46 +0530

    fixed major issues

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -5,43 +5,48 @@ import SkillsIcon from '../assets/skills.png'
 import { useNavigate } from 'react-router-dom'
 export default function ProfileBanner() {
 
-    const navigate=useNavigate();   
+    const navigate = useNavigate();
 
     function navigateSkill() {
         navigate('/skills');
     }
 
-  return (
-    <div className='profile'>
-        <div className='profile-description'>
-            <div className="user-name">
-                <h1>Ashwin Khowala - Full Stack Web Developer</h1>
-            </div>
-            <div className="user-description">
-                <p>
-                I’m a passionate Full Stack Web Developer with a knack for building intuitive and dynamic web applications. Currently exploring the MERN stack, I love solving complex problems with clean, efficient code. Beyond coding, I’m diving into data structures and algorithms to sharpen my problem-solving skills.
-                <br />
-                When I’m not immersed in code, you’ll find me exploring innovative ideas in social entrepreneurship, blending technology with impactful solutions.
-                <br />
-                Let’s collaborate and build something amazing together!
-                </p>
-            </div>
-            <div className="buttons">
-                <button className="resume-btn  button">
-                    <img src={ResumeIcon} className="resume-icon" alt="" srcset=""/>
+    return (
+        <div className='profile'>
+            <div className='profile-description'>
+                <div className="user-name">
+                    <h1>Ashwin Khowala - Full Stack Web Developer</h1>
+                </div>
+                <div className="user-description">
                     <p>
-                        Resume
+                        I’m a Full Stack Web Developer with expertise in the MERN stack (MongoDB, Express, React, Node.js).
+                        <br />
+                        I focus on building intuitive and dynamic web applications, designing seamless user interfaces, and ensuring strong backend functionality.
+                        <br />
+                        Currently, I'm enhancing my skills in building scalable applications.
+                        <br />
+                        In addition to my web development skills, I’m diving deep into data structures and algorithms to strengthen my problem-solving abilities and optimize code performance.
+                        <br />
+                        I’m passionate about clean, efficient code and always strive to build applications that are both robust and maintainable.
                     </p>
-                </button>
-                <button className="skills-btn button" onClick={()=>navigateSkill()}>
-                    <img src={SkillsIcon} className='skills-icon' alt="" srcset="" />
-                    <p>
-                        Skills
-                    </p>
-                </button>
-            </div>
-        </div>
+                </div>
 
-    </div>
-  )
+                <div className="buttons">
+                    <button className="resume-btn  button">
+                        <img src={ResumeIcon} className="resume-icon" alt="" srcset="" />
+                        <p>
+                            Resume
+                        </p>
+                    </button>
+                    <button className="skills-btn button" onClick={() => navigateSkill()}>
+                        <img src={SkillsIcon} className='skills-icon' alt="" srcset="" />
+                        <p>
+                            Skills
+                        </p>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    )
 }
