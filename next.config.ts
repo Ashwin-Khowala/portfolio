de5762cf2362ab866600ce641e68d4d18079be5a
Ashwin-Khowala <ashwinkhowala1@gmail.com>
2026-06-15 19:08:21 +0530

    redirect to diff domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ashwinkhowala.com',
+          },
+        ],
+        destination: 'https://ashwin.clubfyndr.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.ashwinkhowala.com',
+          },
+        ],
+        destination: 'https://ashwin.clubfyndr.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
